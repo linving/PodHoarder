eda34488dee08ad04cddba8b154f885897f92904
PodHoardBjas <sebastian.mikael.andersson@gmail.com>
2013-04-19 09:58:15 +0200

    Swipe functionality & layout files

---
+++ src/com/podhoarderproject/podhoarder/gui/FeedFragment.java
@@ -0,0 +1,30 @@
+package com.podhoarderproject.podhoarder.gui;
+
+import com.podhoarderproject.podhoarder.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+ 
+/**
+ * 
+ * @author Sebastian Andersson
+ * 2013-04-17
+ */
+public class FeedFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        if (container == null) 
+        {
+            return null;
+        }
+        else
+        {
+        	return (LinearLayout)inflater.inflate(R.layout.fragment_feed, container, false);
+        }
+    }
+}
